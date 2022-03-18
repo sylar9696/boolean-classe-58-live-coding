1930206de6734b109855733808f72d54c1697d9c
Alessandro Lodigiani <46444351+sylar9696@users.noreply.github.com>
2022-03-18 09:29:29 +0100

    finish

---
+++ js-while/main.js
@@ -0,0 +1,28 @@
+// for( i = 0; i < 5; i++ ){
+//   console.log( i );
+// }
+
+
+//con ciclo while
+// let i = 0;
+
+// while( i < 5 ){
+//   console.log( i );
+//   i++;
+// }
+
+// let y=5;
+
+// do{
+//   console.log( y ); //5
+//   y++ //6
+//   console.log( y ); //6
+// } while ( y < 4 ); //false
+
+let y = 5;
+
+while( y < 4 ){ //false
+  console.log( y ); //5
+  y++ //6
+  console.log( y ); //6
+}
